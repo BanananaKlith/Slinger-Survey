ce62d763731a7918704883dc5e588ddd6b0a1c0f
Slinger Pasta <yhylhl.10@gmail.com>
2023-10-29 21:38:34 +0100

    v4

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 import CreateSurvey from './Views/createSurvey';
 import SavedSurvey from './Views/savedSurvey';
 import PreviewSurvey from './Views/previewSurvey';
